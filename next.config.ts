ba6c0ec66e78888e91066e1efa44a57c67ba2a42
dental king <ejqjr3377@naver.com>
2025-04-18 17:33:31 +0900

    202504180511

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  eslint: {
+    // ESLint 검사를 빌드 과정에서 건너뜁니다
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
